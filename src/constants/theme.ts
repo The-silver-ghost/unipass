bfdf1ae21790b3774f492c9f9ebb3747f206577f
Harvind Sethu Pathy <harvindddddd@gmail.com>
2026-06-03 15:13:06 +0800

    Added theme

---
+++ src/constants/theme.ts
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    bg: '#1a0b0b',
+    bgDark: '#0a0000',
+    brightRed: '#db2c2c',
+    white: '#ffffff',
+  },
+  glassmorphism: {
+    backgroundColor: 'rgba(255, 255, 255, 0.05)',
+    borderWidth: 1,
+    borderColor: 'rgba(255, 255, 255, 0.1)',
+    borderRadius: 16,
+  }
+};
